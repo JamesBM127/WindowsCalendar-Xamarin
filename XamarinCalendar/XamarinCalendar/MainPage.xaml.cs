972dec9917d8872e04092916962fa4715b683eda
Jameson Brandao Martins <jamesonmartins127@gmail.com>
2021-02-13 19:22:03 -0400

    Date and hour

--- XamarinCalendar/XamarinCalendar/MainPage.xaml.cs
+++ XamarinCalendar/XamarinCalendar/MainPage.xaml.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using Xamarin.Forms;
 
 namespace XamarinCalendar
 {
     public partial class MainPage : ContentPage
     {
+        public DateTime Date { get; set; }
+
+        private void TodayFunc()
+        {
+            this.Date = DateTime.Now;
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                this.Date = DateTime.Now;
+
+                Hour.Text = Date.ToString("HH:mm:ss");
+                DayOfTheWeek.Text = Date.DayOfWeek.ToString() + ", " + Date.ToString("dd") + " de " + Date.ToString("MMMM") + " de " + Date.ToString("yyyy");
+                return true;
+            });
+        }
+
         public MainPage()
         {
             InitializeComponent();
+
+            TodayFunc();
+
+
         }
     }
 }
